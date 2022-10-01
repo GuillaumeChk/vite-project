3f2720074ce79aee6ded45f79a968b153e6d6704
GuillaumeChk <guillaume.chekhab39@gmail.com>
2022-10-01 16:33:44 +0200

    first commit

---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img
+    v-for="url in imagesUrl"
+    :key="url.name"
+      :src="getImageUrl(url.name)"
+      style="width: 50px; height: 50px"
+    />
+  </div>
+</template>
+
+<script setup>
+
+function getImageUrl(subPath) {
+  return new URL(`${subPath}`, import.meta.url).href;
+}
+
+let imagesUrl =  import.meta.glob("../assets/images/*.(jpg|JPG|png|PNG|svg)", { as: "url" });
+
+</script>
